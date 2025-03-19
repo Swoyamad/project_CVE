a29d29a92fe48f4d61888bba15231d0e38d0b9ba
Swoyam Adhikari <SwoyamAdhikari@example.com>
2025-03-19 11:56:43 -0400

    Head Title changed

--- resources/js/Pages/News/Index.jsx
+++ resources/js/Pages/News/Index.jsx
@@ -9,7 +9,7 @@ export default function HackerNewsIndex({
 }) {
     return (
         <div className="container mx-auto px-4">
-            <Head title="SinghIT News" />
+            <Head title="HackerNews" />
 
             <div className="flex justify-between items-center mb-6">
                 <h1 className="text-3xl font-bold">SinghIT </h1>
